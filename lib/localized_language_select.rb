d4b3e88ff77988dad661f48429843dd6c90453c1
Indrek Juhkam <indrek@urgas.eu>
2012-04-23 17:43:23 +0300

    Load Formtastic only if it is present

--- lib/localized_language_select.rb
+++ lib/localized_language_select.rb
@@ -120,23 +120,24 @@ module ActionView
   end
 end
 
-module Formtastic
-  module Inputs
-    class LanguageInput
-      include Base
+if defined?(Formtastic)
+  module Formtastic
+    module Inputs
+      class LanguageInput
+        include Base
 
-      def to_html
-        raise "To use the :language input, please install a language_select plugin, like this one: https://github.com/jeanmartin/localized_language_select/blob/master/lib/localized_language_select.rb" unless builder.respond_to?(:language_select)
-        input_wrapping do
-          label_html <<
-          builder.language_select(method, priority_languages, input_options, input_html_options)
+        def to_html
+          raise "To use the :language input, please install a language_select plugin, like this one: https://github.com/jeanmartin/localized_language_select/blob/master/lib/localized_language_select.rb" unless builder.respond_to?(:language_select)
+          input_wrapping do
+            label_html <<
+            builder.language_select(method, priority_languages, input_options, input_html_options)
+          end
         end
-      end
 
-      def priority_languages
-        options[:priority_languages] || []#builder.priority_languages
+        def priority_languages
+          options[:priority_languages] || []#builder.priority_languages
+        end
       end
     end
   end
 end
-
