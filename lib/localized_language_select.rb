f286b67034968464cf2df42a7e195f80105c0499
Indrek Juhkam <indrek@urgas.eu>
2012-04-23 17:42:34 +0300

    Clear whitespace

--- lib/localized_language_select.rb
+++ lib/localized_language_select.rb
@@ -1,20 +1,20 @@
 # = LocalizedLanguageSelect
-# 
+#
 # View helper for displaying select list with languages:
-# 
+#
 #     localized_language_select(:user, :language)
-# 
+#
 # Works just like the default Rails' +country_select+ plugin, but stores languages as
 # language *codes*, not *names*, in the database.
-# 
+#
 # You can easily translate language codes in your application like this:
 #     <%= I18n.t @user.language, :scope => 'languages' %>
-# 
+#
 # Uses the Rails internationalization framework (I18n) for translating the names of languages.
-# 
+#
 # Use Rake task <tt>rake import:language_select 'de'</tt> for importing language names
 # from Unicode.org's CLDR repository (http://www.unicode.org/cldr/data/charts/summary/root.html)
-# 
+#
 # The code borrows heavily from the LocalizedCountrySelect plugin.
 # See http://github.com/karmi/localized_country_select
 #
@@ -27,23 +27,23 @@ module LocalizedLanguageSelect
     # for <tt><option></tt> tags
     def localized_languages_array options = {}
       res = []
-      list = I18n.translate(:languages).each do |key, value| 
+      list = I18n.translate(:languages).each do |key, value|
         res << [value, key.to_s] if include_language?(key.to_s, options)
       end
       res.sort_by { |country| country.first.parameterize }
     end
-    
-    def include_language?(key, options)                                           
-      if options[:only] 
+
+    def include_language?(key, options)
+      if options[:only]
         return options[:only].include?(key)
-      end      
-      if options[:except] 
+      end
+      if options[:except]
         return !options[:except].include?(key)
       end
-      true      
-    end      
-    
-    
+      true
+    end
+
+
     # Return array with codes and localized language names for array of language codes passed as argument
     # == Example
     #   priority_languages_array([:de, :fr, :en])
@@ -60,7 +60,7 @@ module ActionView
 
     module FormOptionsHelper
 
-      # Return select and option tags for the given object and method, using +localized_language_options_for_select+ 
+      # Return select and option tags for the given object and method, using +localized_language_options_for_select+
       # to generate the list of option tags. Uses <b>language code</b>, not name as option +value+.
       # Language codes listed as an array of symbols in +priority_languages+ argument will be listed first
       # TODO : Implement pseudo-named args with a hash, not the "somebody said PHP?" multiple args sillines
@@ -81,7 +81,7 @@ module ActionView
       end
       alias_method :language_select_tag, :localized_language_select_tag
 
-      # Returns a string of option tags for languages according to locale. Supply the language code in lower-case ('en', 'de') 
+      # Returns a string of option tags for languages according to locale. Supply the language code in lower-case ('en', 'de')
       # as +selected+ to have it marked as the selected option tag.
       # Language codes listed as an array of symbols in +priority_languages+ argument will be listed first
       def localized_language_options_for_select(selected = nil, priority_languages = nil, options = {})
@@ -93,7 +93,7 @@ module ActionView
         return language_options + options_for_select(LocalizedLanguageSelect::localized_languages_array(options), selected)
       end
       alias_method :language_options_for_select, :localized_language_options_for_select
-      
+
     end
 
     class InstanceTag
@@ -109,7 +109,7 @@ module ActionView
         )
       end
     end
-    
+
     class FormBuilder
       def localized_language_select(method, priority_languages = nil, options = {}, html_options = {})
         @template.localized_language_select(@object_name, method, priority_languages, options.merge(:object => @object), html_options)
@@ -132,7 +132,7 @@ module Formtastic
           builder.language_select(method, priority_languages, input_options, input_html_options)
         end
       end
-      
+
       def priority_languages
         options[:priority_languages] || []#builder.priority_languages
       end
